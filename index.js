38c8b8004faafcfebc949f062d3ba332c6fa6c83
Mikhail Novikov <sigod.mail@gmail.com>
2013-10-11 01:09:56 +0300

    implementation;

---
+++ index.js
@@ -0,0 +1,17 @@
+var through = require('through');
+
+module.exports = function (file) {
+	if (!/\.template$/.test(file)) return through();
+
+	var buffer = '';
+
+	return through(
+		function write(chunk) {
+			buffer += chunk;
+		},
+		function end() {
+			this.queue("module.exports='" + buffer.replace(/'/g, "\\'").replace(/(\r\n|\n|\r)/g, '\\\n') + "';");
+			this.queue(null);
+		}
+	);
+};
